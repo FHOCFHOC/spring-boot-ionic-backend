f00e41d630061283d4a93bccf3b9ca860c462b70
FHOCFHOC <backintheusa@gmail.com>
2019-01-14 14:39:28 -0200

    Produto e associacao muitos para muitos

--- src/main/java/com/corinto/mc/McApplication.java
+++ src/main/java/com/corinto/mc/McApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.corinto.mc.domain.Categoria;
+import com.corinto.mc.domain.Produto;
 import com.corinto.mc.repositories.CategoriaRepository;
+import com.corinto.mc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class McApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriarepository;
+	@Autowired
+	private ProdutoRepository produtorepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
@@ -26,7 +30,19 @@ public class McApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "informatica");
 		Categoria cat2 = new Categoria(null, "escritorio");
 		
+		Produto p1 = new Produto(null, "produto1", 2000.00);
+		Produto p2 = new Produto(null, "produto2", 3000.00);
+		Produto p3 = new Produto(null, "produto3", 4000.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
+		produtorepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
