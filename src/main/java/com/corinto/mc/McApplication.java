212f9532e1f7a22b692174f9a5de4bb5a77032d7
FHOCFHOC <backintheusa@gmail.com>
2019-01-14 13:46:06 -0200

    Criando operacao de instanciacao

--- src/main/java/com/corinto/mc/McApplication.java
+++ src/main/java/com/corinto/mc/McApplication.java
@@ -1,13 +1,32 @@
 package com.corinto.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.corinto.mc.domain.Categoria;
+import com.corinto.mc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class McApplication {
+public class McApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriarepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
