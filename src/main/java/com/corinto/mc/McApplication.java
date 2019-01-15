1fce08e1ec3b14ac7c693c565f7fef78772fb7d4
FHOCFHOC <backintheusa@gmail.com>
2019-01-15 13:35:50 -0200

    Estado e cidade

--- src/main/java/com/corinto/mc/McApplication.java
+++ src/main/java/com/corinto/mc/McApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.corinto.mc.domain.Categoria;
+import com.corinto.mc.domain.Cidade;
+import com.corinto.mc.domain.Estado;
 import com.corinto.mc.domain.Produto;
 import com.corinto.mc.repositories.CategoriaRepository;
+import com.corinto.mc.repositories.CidadeRepository;
+import com.corinto.mc.repositories.EstadoRepository;
 import com.corinto.mc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class McApplication implements CommandLineRunner{
 	private CategoriaRepository categoriarepository;
 	@Autowired
 	private ProdutoRepository produtorepository;
+	@Autowired
+	private EstadoRepository estadorepository;
+	@Autowired
+	private CidadeRepository cidaderepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
@@ -43,6 +51,20 @@ public class McApplication implements CommandLineRunner{
 		
 		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
 		produtorepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadorepository.saveAll(Arrays.asList(est1, est2));
+		cidaderepository.saveAll(Arrays.asList(c1, c2, c3));
+		
 	}
 
 }
