41d026eb4deff7848f6da7d140d3eb8ca7a91d2e
FHOCFHOC <backintheusa@gmail.com>
2019-01-18 21:56:45 -0200

    Atualizando categoria com PUT

--- src/main/java/com/corinto/mc/services/CategoriaService.java
+++ src/main/java/com/corinto/mc/services/CategoriaService.java
@@ -9,7 +9,6 @@ import com.corinto.mc.domain.Categoria;
 import com.corinto.mc.repositories.CategoriaRepository;
 import com.corinto.mc.services.exceptions.ObjectNotFoundException;
 
-
 @Service
 public class CategoriaService {
 
@@ -19,11 +18,16 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
 		return repo.save(obj);
 	}
 }
