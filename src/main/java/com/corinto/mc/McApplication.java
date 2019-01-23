e33b03ccf36a8ab36b962f092dce87c08c106be8
FHOCFHOC <backintheusa@gmail.com>
2019-01-23 13:25:27 -0200

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/corinto/mc/McApplication.java
+++ src/main/java/com/corinto/mc/McApplication.java
@@ -63,6 +63,11 @@ public class McApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "informatica");
 		Categoria cat2 = new Categoria(null, "escritorio");
+		Categoria cat3 = new Categoria(null, "cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "eletronicos");
+		Categoria cat5 = new Categoria(null, "jardinagem");
+		Categoria cat6 = new Categoria(null, "decoracao");
+		Categoria cat7 = new Categoria(null, "perfumaria");
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -75,7 +80,7 @@ public class McApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
