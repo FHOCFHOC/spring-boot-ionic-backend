ad4ed236b243f26fce58fdc3c5ad0d41780124ca
FHOCFHOC <backintheusa@gmail.com>
2019-02-13 13:45:06 -0200

    Validacao customizada

--- src/main/java/com/corinto/mc/dto/ClienteDTO.java
+++ src/main/java/com/corinto/mc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.corinto.mc.domain.Cliente;
+import com.corinto.mc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
