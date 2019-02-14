721e3cb0f7fee4eaf6b5035aaf8116dedf360305
FHOCFHOC <backintheusa@gmail.com>
2019-02-14 14:39:09 -0200

    Inserindo Pedido

--- src/main/java/com/corinto/mc/resources/PedidoResource.java
+++ src/main/java/com/corinto/mc/resources/PedidoResource.java
@@ -1,12 +1,18 @@
 package com.corinto.mc.resources;
 
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.corinto.mc.domain.Pedido;
 import com.corinto.mc.services.PedidoService;
@@ -25,4 +31,11 @@ public class PedidoResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
