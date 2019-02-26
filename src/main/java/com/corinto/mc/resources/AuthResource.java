19453d18da50ec55750ca49e91351c5f0a09478e
FHOCFHOC <backintheusa@gmail.com>
2019-02-25 21:21:58 -0300

    Esqueci a senha

--- src/main/java/com/corinto/mc/resources/AuthResource.java
+++ src/main/java/com/corinto/mc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.corinto.mc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.corinto.mc.dto.EmailDTO;
 import com.corinto.mc.security.JWTUtil;
 import com.corinto.mc.security.UserSS;
+import com.corinto.mc.services.AuthService;
 import com.corinto.mc.services.UserService;
 
 @RestController
@@ -18,7 +22,10 @@ public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
-	
+
+	@Autowired
+	private AuthService service;
+
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
@@ -28,4 +35,9 @@ public class AuthResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
+		return ResponseEntity.noContent().build();
+	}
 }
