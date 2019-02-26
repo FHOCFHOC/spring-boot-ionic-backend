a9123090bf6a2289186f5d88b54d86e77c8d417b
FHOCFHOC <backintheusa@gmail.com>
2019-02-26 12:59:26 -0300

    Salvando primeiro aruqivo no s3

--- src/main/java/com/corinto/mc/McApplication.java
+++ src/main/java/com/corinto/mc/McApplication.java
@@ -1,13 +1,17 @@
 package com.corinto.mc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.corinto.mc.services.S3Service;
+
 @SpringBootApplication
 public class McApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
@@ -15,6 +19,7 @@ public class McApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("/Users/fernandohenriqueoliveiracosta/Desktop/foto.jpg");
 	}
 
 }
